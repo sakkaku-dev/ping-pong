19597e8c480ffa200d516d4925f956d6288b2863
myin <minyin142p@gmail.com>
2021-03-27 10:23:22 +0100

    Fix multiply power up spawning one too much (Close #13)

--- scenes/powerups/MultiplyBall.gd
+++ scenes/powerups/MultiplyBall.gd
@@ -1,10 +1,11 @@
 extends PowerUp
 
+# Includes the existing ball, meaning with 3 only 2 extra balls will be spawned
 export var multiply_count = 3
 
 func on_pickup(ball: Ball) -> void:
 	var rotate_value = 360 / multiply_count
-	for i in range(1, multiply_count + 1):
+	for i in range(1, multiply_count):
 		var copy: Ball = ball.copy()
 		copy.set_physics_process(true)
 		copy.velocity = copy.velocity.rotated(deg2rad(rotate_value) * i)

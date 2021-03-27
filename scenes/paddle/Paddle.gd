31083e53e8a5ae178002cd0039b7ad24bf478dfa
myin <minyin142p@gmail.com>
2021-03-27 11:56:33 +0100

    Paddle, prevent moving in locked direction (Close #10)

--- scenes/paddle/Paddle.gd
+++ scenes/paddle/Paddle.gd
@@ -8,7 +8,12 @@ export var speed = 200
 
 var ctrl: PaddleController
 var velocity = Vector2.ZERO
-var lock_direction = Vector2.ZERO
+var lock_direction = Vector2.ZERO setget set_lock_direction
+var lock_position = Vector2.ZERO
+
+func set_lock_direction(dir: Vector2) -> void:
+	lock_direction = dir
+	lock_position = global_position
 
 func _physics_process(_delta):
 	if not ctrl: return
@@ -16,12 +21,20 @@ func _physics_process(_delta):
 	var motion = ctrl.get_motion(self)
 	velocity = motion * speed
 
-	if lock_direction.y != 0:
-		velocity.x = 0
-	elif lock_direction.x != 0:
-		velocity.y = 0
-
+	velocity = update_lock_direction(velocity, Vector2(0, 0))
 	velocity = move_and_slide(velocity)
+
+	# make sure it does not move in a locked direction
+	global_position = update_lock_direction(global_position, lock_position)
+
+
+func update_lock_direction(vec: Vector2, new: Vector2) -> Vector2:
+	if lock_direction.y != 0:
+		vec.x = new.x
+	elif lock_direction.x != 0:
+		vec.y = new.y
+	return vec
+
 
 func get_lock_axis_value(vec: Vector2):
 	if lock_direction.y != 0:

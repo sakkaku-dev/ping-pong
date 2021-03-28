7a0a1e398d572f58d0408c311309b5a7a47d5a45
myin <minyin142p@gmail.com>
2021-03-28 10:44:15 +0200

    release script, Handle errors

--- scripts/build/release.sh
+++ scripts/build/release.sh
@@ -4,10 +4,14 @@
 #godot --export "Linux" ./build/linux/main.x86_64
 godot --export "HTML5" ./build/web/index.html
 
+CURRENT_BRANCH="$(git branch --show-current)"
 if git checkout gh-pages; then
     cp ./build/web/* .
     git add -u
 
-    git commit -m 'Deploy via script'
-    git push
+    if git commit -m 'Deploy via script'; then
+        git push
+    fi
+
+    git checkout $CURRENT_BRANCH
 fi

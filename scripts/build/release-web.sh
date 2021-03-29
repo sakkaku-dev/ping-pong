dbed5d86f09df0e82a43be355c927a63a6aa38e9
myin <minyin142p@gmail.com>
2021-03-29 17:42:34 +0200

    Remove gh-pages temp folder

--- scripts/build/release-web.sh
+++ scripts/build/release-web.sh
@@ -3,7 +3,11 @@
 # Web version only works if exported from inside the godot engine
 git clone --branch gh-pages git@github.com:sakkaku-dev/ping-pong.git gh-pages
 cp ./build/web/* gh-pages -R
+
 cd gh-pages
 git add -A
 git commit -m 'Deploy via script'
 git push
+
+cd ..
+rm gh-pages -R

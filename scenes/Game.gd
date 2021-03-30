a29c0fcfc02f405fe7fc81a9a642281c9ae43734
myin <minyin142p@gmail.com>
2021-03-30 17:32:06 +0200

    Prevent join after game start (Close #21)

--- scenes/Game.gd
+++ scenes/Game.gd
@@ -4,6 +4,7 @@ onready var world = $World
 onready var start_menu = $CanvasLayer/MarginContainer/CenterContainer/StartMenu
 onready var bgm = $BGM
 
+var started = false
 var host_input: PlayerInput
 
 func _ready():
@@ -11,13 +12,14 @@ func _ready():
 	start_menu.show()
 
 func _unhandled_input(event: InputEvent):
-	if event.is_action("join"):
+	if not started and event.is_action("join"):
 		var input = world.spawn_player(event)
 		if not host_input:
 			host_input = input
 
 
 func _on_Start_pressed():
+	started = true
 	start_menu.hide()
 	world.start_game()
 	bgm.play()
